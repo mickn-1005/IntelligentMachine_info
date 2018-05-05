3c63113aeea6dc7e141d0abc0652129b871f9f7b
mickn-1005 <wataru1005mikkun@gmail.com>
2018-05-05 19:27:18 +0900

    okok

--- report1/main.jl
+++ report1/main.jl
@@ -9,7 +9,7 @@ rnd = MersenneTwister(111)
 # define constant
 const theta = 0.
 const imgdim = 5  # 画像の一辺画素
-const itr = 1000
+const itr = 10
 
 #======================
 = define functions... =
@@ -97,7 +97,7 @@ function pat_img(pat_ar::AbstractArray, dim::Int, title::String="title")
     imgs = [heatmap(ppat, bar_width=false, title=title) for ppat in pat_ar]
     return imgs #パターン画像のリストを戻す
 end
-pat_img(weight::AbstractMatrix) = heatmap(weight, bar_width=false,size=(10,10), title="weight")
+pat_img(weight::AbstractMatrix) = heatmap(weight, bar_width=false, size=(500,500), title="weight")
 out_plot(imt, imi, imo, ind::Int=1) = plot(imt[ind],imi[ind], imo[ind], layout=(1,3),size=(600,200))
 
 function HopfieldNet(pats::AbstractVector, noise_ratio::AbstractFloat)
@@ -114,6 +114,7 @@ function hnn_eval(pats, noise_ratio=0.1)
     eng = zeros(size(pats)[1])
     # 正誤率初期化
     tf_cnt = zeros(size(pats)[1])
+    anti = zeros(size(pats)[1])
     weight = zeros(imgdim^2, imgdim^2)
 
     for i in 1:itr
@@ -122,15 +123,19 @@ function hnn_eval(pats, noise_ratio=0.1)
             if pats[i] == pats_out[i]
                 tf_cnt[i] +=1
             end
+            if pats[i] == -pats_out[i]
+                anti[i] += 1
+            end
         end
     end
 
     tf_cnt = tf_cnt / itr
+    anti = anti / itr
 
     imgs_train = pat_img(pats, imgdim, "train_data")
     imgs_in = pat_img(pats_in, imgdim, "test_data")
     imgs_out = pat_img(pats_out, imgdim, "output")
-    return weight, imgs_train, imgs_in, imgs_out, tf_cnt
+    return weight, imgs_train, imgs_in, imgs_out, tf_cnt, anti
 end
 
 #=======================
@@ -139,6 +144,7 @@ end
 
 function remember()
     println("remember initial image...")
+    weight = zeros(imgdim^2, imgdim^2)
     noises = collect(0.04:0.04:0.2)
     pat = make_train_data(1,imgdim)
     imt = []
@@ -146,12 +152,12 @@ function remember()
     imo = []
 
     for (nz,i) in zip(noises, 1:size(noises)[1])
-        weight, imgs_train, imgs_in, imgs_out, tf_cnt = hnn_eval(pat, nz)
+        weight, imgs_train, imgs_in, imgs_out, tf_cnt, anti = hnn_eval(pat, nz)
         push!(imt, imgs_train)
         push!(imi, imgs_in)
         push!(imo, imgs_out)
     end
-    return imt, imi, imo
+    return imt, imi, imo, weight
 end
 
 
@@ -159,16 +165,16 @@ function many_pats()
     println("memorize many patterns...")
     pat_size = [1,2,3,4,5,6,7]
     tfs = zeros(size(pat_size)[1])
-    main_itr = 10
+    main_itr = 100
     for j in 1:main_itr
         for i in pat_size
             # 評価画像の生成
             pats = make_train_data(i, imgdim)
-            weight, imgs_train, imgs_in, imgs_out, tf_cnt = hnn_eval(pats)
+            weight, imgs_train, imgs_in, imgs_out, tf_cnt, anti = hnn_eval(pats)
             tfs[i] += mean(tf_cnt)   #平均正解率
         end
     end
-    tf_cnt = tf_cnt/main_itr
+    tfs = tfs/main_itr * 100
     return tfs
 end
 
@@ -177,34 +183,53 @@ function many_noise()
     noises = [0,0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,1]
     tfs2 = zeros(11)
     tfs4 = zeros(11)
-    pats2 = make_train_data(2, imgdim)
-    pats4 = make_train_data(4, imgdim)
+    anti2 = zeros(11)
+    anti4 = zeros(11)
+    main_itr = 100
 
-    for nz in 0:10
-        weight, imgs_train, imgs_in, imgs_out, tf_cnt = hnn_eval(pats2, nz/10)
-        tfs2[nz+1] += mean(tf_cnt)
-        weight, imgs_train, imgs_in, imgs_out, tf_cnt = hnn_eval(pats4, nz/10)
-        tfs4[nz+1] += mean(tf_cnt)
+    for j in 1:main_itr
+        pats2 = make_train_data(2, imgdim)
+        pats4 = make_train_data(4, imgdim)
+        for nz in 0:10
+            weight, imgs_train, imgs_in, imgs_out, tf_cnt, anti = hnn_eval(pats2, nz/10)
+            tfs2[nz+1] += mean(tf_cnt)
+            anti2[nz+1] += mean(anti)
+            weight, imgs_train, imgs_in, imgs_out, tf_cnt, anti = hnn_eval(pats4, nz/10)
+            tfs4[nz+1] += mean(tf_cnt)
+            anti4[nz+1] += mean(anti)
+        end
     end
-    return tfs2, tfs4, noises
+    tfs2 = tfs2/main_itr * 100
+    tfs4 = tfs4/main_itr * 100
+    anti2 = anti2/main_itr * 100
+    anti4 = anti4/main_itr * 100
+    return tfs2, tfs4, noises, anti2, anti4
 end
 
 # main routine
-imt, imi, imo = @time remember()
+imt, imi, imo, weight = @time remember()
 tfs = @time many_pats()
-tfs2, tfs4, noises = @time many_noise()
-out1 = out_plot(imt[1], imi[1], imo[1])
+tfs2, tfs4, noises, anti2, anti4 = @time many_noise()
+out_plot(imt[1], imi[1], imo[1])
 savefig("out1.png")
-out2 = out_plot(imt[2], imi[2], imo[2])
+out_plot(imt[2], imi[2], imo[2])
 savefig("out2.png")
-out3 = out_plot(imt[3], imi[3], imo[3])
+out_plot(imt[3], imi[3], imo[3])
 savefig("out3.png")
-out4 = out_plot(imt[4], imi[4], imo[4])
+out_plot(imt[4], imi[4], imo[4])
 savefig("out4.png")
-out5 = out_plot(imt[5], imi[5], imo[5])
+out_plot(imt[5], imi[5], imo[5])
 savefig("out5.png")
-out6 = plot(tfs)
+pat_img(weight)
+savefig("weight.png")
+plot(tfs,xlabel="No. of patterns", ylabel="accuracy[%]",ylims=[0,100])
 savefig("out6.png")
-plot(noises,tfs2, label="two type memorized")
-out7 = plot!(noises,tfs4, label="four type memorized")
+plot(noises,tfs2, label="two type memorized",xlabel="noise ratio", ylabel="accuracy[%]")
+plot!(noises,tfs4, label="four type memorized")
 savefig("out7.png")
+
+plot(noises,tfs2, label="2-mem",xlabel="noise ratio", ylabel="accuracy[%]")
+plot!(noises,tfs4, label="4-mem")
+plot!(noises,anti2, label="2-anti")
+plot!(noises,anti4, label="4-anti")
+savefig("out8.png")
